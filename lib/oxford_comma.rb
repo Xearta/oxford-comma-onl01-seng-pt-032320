49d4ea6bb5b3822b7fc75572a31779eb5bf8422f
Tanner Townsend <rsmcwow@gmail.com>
2020-03-30 00:49:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return message
   elsif array.length > 1
     message = array[0...-1].join
-    message += " and #{array[-1]"
+    message += " and #{array[-1]}"
     return message
   end
 end
